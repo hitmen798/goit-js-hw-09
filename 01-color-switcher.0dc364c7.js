11a5c5da46c2a860ce59a07c94f37a29ef82e70f
hitmen798 <124578265+hitmen798@users.noreply.github.com>
2023-09-06 15:53:14 +0000

    Deploying to gh-pages from @ hitmen798/goit-js-hw-09@d8384f283e34a5b23add5d6151a1980541461355 🚀

---
+++ 01-color-switcher.0dc364c7.js
@@ -0,0 +1,2 @@
+let e;const t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(()=>{e||(t.disabled=!0,d.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3))})),d.addEventListener("click",(()=>{e&&(clearInterval(e),e=null,t.disabled=!1,d.disabled=!0)}));
+//# sourceMappingURL=01-color-switcher.0dc364c7.js.map
