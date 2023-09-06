468ba471dd77f8d26e59117d0865a0cabb1b7d41
hitmen798 <124578265+hitmen798@users.noreply.github.com>
2023-09-06 17:46:39 +0200

    123

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+let intervalId;
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', () => {
+  if (!intervalId) {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  }
+});
+// 9
